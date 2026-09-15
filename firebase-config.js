db05c85479b264d1ade53691a5f8162eb3264f75
auraultrax <xauraultra@gmail.com>
2026-09-15 21:45:37 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAnmZghkJEfxKNcvLDRilXP-iQc3lMMLrc",
+  authDomain: "sisteym-14312.firebaseapp.com",
+  projectId: "sisteym-14312",
+  storageBucket: "sisteym-14312.firebasestorage.app",
+  messagingSenderId: "498532288088",
+  appId: "1:498532288088:web:8d47ef5533f4c9ac946c2d"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
