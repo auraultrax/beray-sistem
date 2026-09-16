af4dd54b309c9b7f43f30b22caff63330f5731de
auraultrax <xauraultra@gmail.com>
2026-09-16 20:31:13 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='yoklama-pwa-v1';
-const FILES=['./','./index.html','./styles.css','./app.js','./firebase-config.js','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request)))})
